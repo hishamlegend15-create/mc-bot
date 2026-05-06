6d0415c57fa3ebd450e9374894fe9565ed3a34a6
hishamlegend15-create <hishamlegend15@gmail.com>
2026-05-06 05:51:24 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+    host: 'APNA_SERVER_IP', // Yahan MagmaNode ka IP likhein
+    port: 25565,           // Yahan port likhein
+    username: 'HishamBot', 
+    version: '1.21.1'
+});
+
+bot.on('spawn', () => {
+    console.log('Bot joined the server!');
+    // Anti-AFK Jump
+    setInterval(() => {
+        bot.setControlState('jump', true);
+        setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 30000);
+});
+
+bot.on('end', () => {
+    console.log('Disconnected! Reconnecting...');
+    setTimeout(() => { location.reload(); }, 5000); 
+});
